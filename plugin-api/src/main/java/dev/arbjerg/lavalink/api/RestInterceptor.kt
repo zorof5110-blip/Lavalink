72dc8150231dba210ff11049bb44c1e7dea43ffb
zorof5110-blip <zorof5110@gmail.com>
2026-03-23 17:16:19 +0530

    Delete plugin-api directory

--- plugin-api/src/main/java/dev/arbjerg/lavalink/api/RestInterceptor.kt
+++
@@ -1,8 +0,0 @@
-package dev.arbjerg.lavalink.api
-
-import org.springframework.web.servlet.HandlerInterceptor
-
-/**
- * This interface allows intercepting HTTP requests to the Lavalink server. Override the methods to add your own logic.
- */
-interface RestInterceptor : HandlerInterceptor
